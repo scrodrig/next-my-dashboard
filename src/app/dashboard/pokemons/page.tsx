b0ee35f61cb3a7cd6231125657f828d16bc2ce2a
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-06-24 15:17:40 +0100

    ```text feat: Refactor PokemonsPage to use PokemonGrid component
    Refactor the PokemonsPage component to use the newly added PokemonGrid component for rendering a grid of Pokemon cards. This improves code organization and reusability by separating the logic for fetching and rendering Pokemon data. The PokemonGrid component takes an array of SimplePokemon objects as props and handles the rendering of individual Pokemon cards. This change also removes the previous implementation of manually rendering Pokemon cards in the PokemonsPage component.

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,4 +1,4 @@
-import { PokemonResponse, SimplePokemon } from "@/app/pokemons";
+import { PokemonGrid, PokemonResponse, SimplePokemon } from "@/app/pokemons";
 import React from "react";
 import Image from "next/image";
 
@@ -23,22 +23,10 @@ export default async function PokemonsPage() {
   return (
     <div className="flex flex-col justify-center items-center">
       <span className="text-5xl my-2">
-        <small className="text-5xl my-2 text-red-600">Static </small> 
+        <small className="text-5xl my-2 text-red-600">Static </small>
         Pokemon List
       </span>
-      <div className="flex flex-wrap gap-10 items-center justify-center">
-        {pokemons.map((pokemon) => (
-          <div key={pokemon.id} className="flex flex-col items-center">
-            <Image
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
-              width={100}
-              height={100}
-              alt={pokemon.name}
-            />
-            <span className="text-lg font-bold capitalize">{pokemon.name}</span>
-          </div>
-        ))}
-      </div>
+      <PokemonGrid pokemons={pokemons} />
     </div>
   );
 }
