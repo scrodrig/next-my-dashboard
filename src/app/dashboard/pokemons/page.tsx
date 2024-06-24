457851cea42945b2589fa60ae723dd3fa68837c5
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-06-24 11:55:08 +0100

    feat: Add PokemonsPage component to dashboard module
    Add the PokemonsPage component to the dashboard module. This component fetches and displays a list of pokemons from the PokeAPI.

---
+++ src/app/dashboard/pokemons/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const getPokemons = async (limit = 20, offset = 0) => {
+  const data = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  ).then((res) => res.json());
+
+  return data;
+};
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons();
+  return <div>{JSON.stringify(pokemons)}</div>;
+}
