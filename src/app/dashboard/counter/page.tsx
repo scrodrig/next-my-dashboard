8f268146f0136038e0b346974f0abed322dd72d0
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-06-24 10:59:05 +0100

    feat: Update CounterPage with CartCounter component
    The CounterPage component is updated to include the CartCounter component from the shopping cart module. This component displays a counter for the number of items in the shopping cart.

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,15 @@
+import { CartCounter } from "@/app/shopping-cart";
+
+export const metadata = {
+  title: "Counter",
+  description: "Counter page",
+};
+
 export default function CounterPage() {
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Shopping cart products</span>
+      <CartCounter value={15}/>
     </div>
   );
 }
