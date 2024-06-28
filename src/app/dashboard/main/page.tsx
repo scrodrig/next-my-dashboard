262dafb9bc080622578b2df190e0bdf9adf91a1c
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-06-28 16:19:19 +0100

    feat: Update MainPage component with SimpleWidget
    Update the MainPage component to include the SimpleWidget component. This enhancement adds a visually appealing and functional widget to the Dashboard page, improving the user interface and providing additional information to the users.

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,7 +1,14 @@
+import { SimpleWidget } from "@/components";
+
 export default function MainPage() {
   return (
-    <div>
-      <h1>Hello Main page</h1>
+    <div className="text-black">
+      <h1 className="mt-2 text-3xl">Dashboard</h1>
+      <span className="text-xl">General Information</span>
+
+      <div className="flex flex-wrap p-2 items-center justify-center">
+        <SimpleWidget />
+      </div>
     </div>
-  );
+  )
 }
