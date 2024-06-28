01570d7fac147b1ee8d221e8f1b62ffc88ef47c7
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-06-28 16:19:14 +0100

    feat: Add Favorites page component
    Add a new component called FavoritesPage to display the user's favorite Pokemon. This component includes a title, a list of favorite Pokemon, and utilizes global state to manage the favorite items. This addition enhances the user interface by providing a dedicated page for managing and viewing favorite Pokemon.

---
+++ src/app/dashboard/favorites/page.tsx
@@ -0,0 +1,14 @@
+import { PokemonGrid } from '@/pokemons'
+import React from 'react'
+
+export default async function FavoritesPage() {
+  return (
+    <div className="p-2 flex flex-col justify-center items-center">
+      <span className="text-5xl my-2">
+        <small className="text-5xl my-2 text-red-600">Global state: </small>
+        Pokemon Favorites
+      </span>
+      <PokemonGrid pokemons={[]} />
+    </div>
+  )
+}
