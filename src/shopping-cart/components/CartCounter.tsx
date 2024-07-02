b28dc62043b82032fbc40609d866972593da883e
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-02 09:43:50 +0100

    refactor: Update CartCounter component to use Redux store for counter state
    Refactor the CartCounter component in the src/shopping-cart/components directory to use the Redux store for managing the counter state. This change replaces the local state management with the use of the useDispatch and useSelector hooks from react-redux. By doing this, the component now dispatches actions to increment and decrement the counter value, and displays the counter value from the Redux store. This improves the consistency and scalability of the state management in the application.

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,12 +1,17 @@
-"use client";
-import { useState } from "react";
+'use client'
+import { useAppDispatch, useAppSelector } from '@/store'
+import { addOne, substractOne } from '@/store/counter/CounterSlice'
 
 interface Props {
-  value?: number;
+  value?: number
 }
 
 export const CartCounter = ({ value = 0 }: Props) => {
-  const [counter, setCounter] = useState(value);
+  // const [counter, setCounter] = useState(value);
+
+  const counter = useAppSelector((state) => state.counter.counter)
+  const dispatch = useAppDispatch()
+
   return (
     <>
       <span className="text-9xl">{counter}</span>
@@ -14,17 +19,15 @@ export const CartCounter = ({ value = 0 }: Props) => {
       <div className="flex">
         <button
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-          onClick={() => setCounter(counter + 1)}
-        >
+          onClick={() => dispatch(addOne())}>
           +1
         </button>
         <button
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-          onClick={() => setCounter(counter - 1)}
-        >
+          onClick={() => dispatch(substractOne())}>
           -1
         </button>
       </div>
     </>
-  );
-};
+  )
+}
