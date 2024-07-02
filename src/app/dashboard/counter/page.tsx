10b3bc22d67bc10112a57a204b62dc6ed264c1c8
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-02 11:25:11 +0100

    feat: Update CartCounter component to display accurate counter value
    Update the CartCounter component in the src/app/dashboard/counter/page.tsx file to display an accurate counter value. The value is now set to 35 instead of 15, improving the accuracy of the displayed shopping cart products count.

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -9,7 +9,7 @@ export default function CounterPage() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
       <span>Shopping cart products</span>
-      <CartCounter value={15}/>
+      <CartCounter value={35}/>
     </div>
   );
 }
