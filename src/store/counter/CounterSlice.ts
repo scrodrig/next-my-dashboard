9daaf204cfed0431f1d5ad5f45b1d0358068b099
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-02 09:53:58 +0100

    ```text refactor: Update CounterSlice to initialize counter state
    Update the CounterSlice module in the src/store/counter directory to include an initCounterState action. This action initializes the counter state with a value provided as a payload. This change ensures that the counter state is properly initialized before any other actions are dispatched.

--- src/store/counter/CounterSlice.ts
+++ src/store/counter/CounterSlice.ts
@@ -2,16 +2,24 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 
 interface CounterState {
   counter: number
+  isReady: boolean
 }
 
 const initialState: CounterState = {
   counter: 5,
+  isReady: false,
 }
 
 const CounterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
+    initCounterState(state, action: PayloadAction<number>) {
+      if (state.isReady) return
+      state.counter = action.payload
+      state.isReady = true
+    },
+
     addOne(state) {
       state.counter += 1
     },
@@ -26,6 +34,6 @@ const CounterSlice = createSlice({
   },
 })
 
-export const {addOne, substractOne, resetCounter} = CounterSlice.actions
+export const { addOne, substractOne, resetCounter, initCounterState } = CounterSlice.actions
 
 export default CounterSlice.reducer
