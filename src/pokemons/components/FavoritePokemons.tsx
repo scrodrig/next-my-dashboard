e3c6a1a2a69b6882f074fc86e52ff7c1fa5aab87
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-02 17:01:07 +0100

    feat: Add NoFavorites component for displaying when there are no favorite pokemons

--- src/pokemons/components/FavoritePokemons.tsx
+++ src/pokemons/components/FavoritePokemons.tsx
@@ -2,11 +2,24 @@
 
 import { useAppSelector } from '@/store'
 import { PokemonGrid } from './PokemonGrid'
+import { useState } from 'react'
+import { IoHeartOutline } from 'react-icons/io5'
 
 export const FavoritePokemons = () => {
   const favoritePokemons = useAppSelector((state) =>
     Object.values(state.pokemons)
   )
 
-  return <PokemonGrid pokemons={favoritePokemons} />
+  const [pokemons, setPokemons] = useState(favoritePokemons)
+
+  return pokemons.length ? <PokemonGrid pokemons={pokemons} /> : NoFavorites()
+}
+
+export const NoFavorites = () => {
+  return (
+    <div className="flex flex-col h-[70vh] items-center justify-center">
+      <IoHeartOutline className="text-9xl text-red-600" />
+      <span className="text-3xl text-red-600">No favorites yet</span>
+    </div>
+  )
 }
