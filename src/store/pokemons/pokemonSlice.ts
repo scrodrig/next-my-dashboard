1d434d905ce094405ee46d0b09ac091c454e47e9
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-02 16:37:53 +0100

    feat: Add pokemonSlice reducer for managing favorite pokemons
    This commit adds the pokemonSlice reducer to the src/store/pokemons/pokemonSlice.ts file. The reducer includes the toggleFavorite action, which allows users to toggle the favorite status of a pokemon. This addition enhances the application by providing a centralized state management solution for managing favorite pokemons.

---
+++ src/store/pokemons/pokemonSlice.ts
@@ -0,0 +1,33 @@
+import { SimplePokemon } from '@/pokemons'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Pokemon } from '../../pokemons/interfaces/pokemon'
+
+interface PokemonState {
+  [key: string]: SimplePokemon
+}
+
+const initialState: PokemonState = {
+  '1': { id: 1, name: 'bulbasaur' },
+  '4': { id: 4, name: 'charmander' },
+}
+
+const pokemonSlice = createSlice({
+  name: 'pokemon',
+  initialState,
+  reducers: {
+    toggleFavorite(state, action: PayloadAction<SimplePokemon>) {
+      const pokemon = action.payload
+      const { id } = pokemon
+      if (!!state[id]) {
+        delete state[id]
+        return
+      }
+
+      state[id] = pokemon
+    },
+  },
+})
+
+export const { toggleFavorite } = pokemonSlice.actions
+
+export default pokemonSlice.reducer
