0f617fd678ab9751c0332dd718cda0d0dfda0264
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-03 16:08:40 +0100

    refactor: Rename localStorateMiddleware to localStorageMiddleware

--- src/store/middlewares/local-storage-middleware.ts
+++ src/store/middlewares/local-storage-middleware.ts
@@ -1,12 +1,12 @@
 import { Action, Dispatch, MiddlewareAPI } from '@reduxjs/toolkit'
-import { RootState } from '@reduxjs/toolkit/query'
+import { RootState } from '..'
 
-export const localStorateMiddleware = (state: MiddlewareAPI) => {
+export const localStorageMiddleware = (state: MiddlewareAPI) => {
   return (next: Dispatch) => (action: Action) => {
     next(action)
 
     if (action.type === 'pokemons/toggleFavorite') {
-      const { pokemons } = state.getState()
+      const { pokemons } = state.getState() as RootState
       localStorage.setItem('favorite-pokemons', JSON.stringify(pokemons))
       return
     }
