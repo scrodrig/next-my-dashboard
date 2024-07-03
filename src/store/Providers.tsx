36f990382146329ff6891f0a61add6075428b184
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-03 16:09:02 +0100

    refactor: Update Providers to set favorite pokemons from local storage
    Update the Providers component to include a useEffect hook that retrieves the favorite pokemons from local storage and dispatches the setFavoritePokemons action to update the store. This change ensures that the favorite pokemons are persisted across page refreshes, providing a seamless user experience.

--- src/store/Providers.tsx
+++ src/store/Providers.tsx
@@ -1,11 +1,19 @@
 'use client'
 import { Provider } from 'react-redux'
 import { store } from './index'
+import { useEffect } from 'react'
+import { setFavoritePokemons } from './pokemons/pokemonSlice'
 
 interface ProvidersProps {
   children: React.ReactNode
 }
 
 export const Providers = ({ children }: ProvidersProps) => {
+  useEffect(() => {
+    const favorites = JSON.parse(
+      localStorage.getItem('favorite-pokemons') ?? '{}'
+    )
+    store.dispatch(setFavoritePokemons(favorites))
+  }, [])
   return <Provider store={store}>{children}</Provider>
 }
